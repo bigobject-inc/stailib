298a0e528e83dab39c5ee57f02e8cabf644abf88
ptoyoohri <leoho@bigobject.io>
2022-02-25 19:00:00 +0800

    add ClientCredentials in APISetting

--- cs/cs.type.go
+++ cs/cs.type.go
@@ -2,9 +2,10 @@ package cs
 
 // APISetting specify Web address and login info
 type APISetting struct {
-	Address      string `json:"address"`
-	Port         string `json:"port"`
-	User         string `json:"user"`
-	Password     string `json:"password"`
-	CacheTimeout int    `json:"cacheTimeout"`
+	Address           string `json:"address"`
+	Port              string `json:"port"`
+	ClientCredentials bool   `json:"clientCredentials"` // if true, then User become client_id,  Password become client_secret
+	User              string `json:"user"`
+	Password          string `json:"password"`
+	CacheTimeout      int    `json:"cacheTimeout"`
 }
